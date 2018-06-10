6cd640d59a8def06efa6e69e2e6b7484c563174e
kana <kana.m.0317@gmail.com>
2018-06-10 21:06:12 +0900

    new file:   .gitignore 	new file:   WebContent/META-INF/MANIFEST.MF 	new file:   WebContent/WEB-INF/views/layout/app.jsp 	new file:   WebContent/WEB-INF/views/tasks/_form.jsp 	new file:   WebContent/WEB-INF/views/tasks/edit.jsp 	new file:   WebContent/WEB-INF/views/tasks/index.jsp 	new file:   WebContent/WEB-INF/views/tasks/new.jsp 	new file:   WebContent/WEB-INF/views/tasks/show.jsp 	new file:   WebContent/css/reset.css 	new file:   WebContent/css/style.css 	new file:   pom.xml 	new file:   src/META-INF/persistence.xml 	new file:   src/controllers/CreateServlet.java 	new file:   src/controllers/DestroyServlet.java 	new file:   src/controllers/EditServlet.java 	new file:   src/controllers/IndexServlet.java 	new file:   src/controllers/NewServlet.java 	new file:   src/controllers/ShowServlet.java 	new file:   src/controllers/UpdateServlet.java 	new file:   src/filters/EncodingFilter.java 	new file:   src/models/Tasks.java 	new file:   src/utils/DBUtil.java 	new file:   tasklist.sql

---
+++ src/models/Tasks.java
@@ -0,0 +1,56 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+@Entity
+
+
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+            )
+})
+
+
+
+
+@Table(name = "tasks")
+public class Tasks{
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
